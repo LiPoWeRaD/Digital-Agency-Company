ba48a7fd0ab1e72f9229c8853563d45230c0380a
LiPoWeRaD <Limon98@list.ru>
2025-04-16 01:19:22 +0300

    fix 11

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,14 +56,14 @@ export default function Home() {
           <p className="mb-[120px] text-lg z-10 text-center">Transform your brand with our innovative digital solutions that captivate and engage your audience.</p>
         </div>
         <ul className="container mx-auto flex flex-col lg:flex-row ">
-          <ServicesCard img="/ServicesDesign.png" title="Design" description="At Squareup, our design team is passionate about creating stunning, 
+          <ServicesCard img="images/ServicesDesign.png" title="Design" description="At Squareup, our design team is passionate about creating stunning, 
                         user-centric designs that captivate your audience and elevate your brand. We believe that great design is not just about aesthetics; 
                         it's about creating seamless and intuitive user experiences." link="#" />
-          <ServicesCard img="/ServicesEngineering.png" title="Engineering" description="Our engineering team combines technical expertise 
+          <ServicesCard img="images/ServicesEngineering.png" title="Engineering" description="Our engineering team combines technical expertise 
                           with a passion for innovation to build robust and scalable digital solutions. 
                           We leverage the latest technologies and best practices to deliver 
                           high-performance applications tailored to your specific needs." link="#" />
-          <ServicesCard img="/ServicesManagement.png" title="Project Management" description="Our experienced project management team ensures that your projects are delivered on time, within budget, 
+          <ServicesCard img="images/ServicesManagement.png" title="Project Management" description="Our experienced project management team ensures that your projects are delivered on time, within budget, 
                           and according to your specifications. 
                           We follow industry-standard methodologies and employ effective communication and collaboration 
                           tools to keep you informed throughout the development process." link="#" />
