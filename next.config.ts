010b5bc8ebde45baaf61e1d5514645946b8b7fcc
LiPoWeRaD <Limon98@list.ru>
2025-04-16 00:50:02 +0300

    fix 8

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/DigitalAgencyCompany",
+  basePath: isProd ? '/Digital-Agency-Company' : '',
+  assetPrefix: isProd ? '/Digital-Agency-Company' : '',
   output: "export", 
   reactStrictMode: true,
 };
